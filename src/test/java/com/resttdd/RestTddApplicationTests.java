4961e218bfad751c9be773ee6d6f241970382126
socra167 <socra167@gmail.com>
2025-02-04 14:06:38 +0900

    chore: 테스트에 `@AutoConfigureMockMvc` 적용

--- src/test/java/com/resttdd/RestTddApplicationTests.java
+++ src/test/java/com/resttdd/RestTddApplicationTests.java
@@ -1,11 +1,13 @@
 package com.resttdd;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@AutoConfigureMockMvc // 내부적으로 네트워크를 사용하지 않고, 네트워크를 사용하는 것처럼 보이게 테스트를 진행한다
 class RestTddApplicationTests {
 
 	@Test
