56dd928967142bf6205ab163349f993215c89be3
socra167 <socra167@gmail.com>
2025-02-04 15:10:17 +0900

    feat: 회원 가입 시 회원이 추가되도록 해 테스트 성공

--- src/main/java/com/resttdd/domain/member/member/controller/ApiV1MemberController.java
+++ src/main/java/com/resttdd/domain/member/member/controller/ApiV1MemberController.java
@@ -1,9 +1,11 @@
 package com.resttdd.domain.member.member.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.resttdd.domain.member.member.service.MemberService;
 import com.resttdd.global.dto.RsData;
 
 import lombok.RequiredArgsConstructor;
@@ -13,8 +15,14 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ApiV1MemberController {
 
+	private final MemberService memberService;
+
+	record JoinReqBody(String username, String password, String nickname) {
+	}
+
 	@PostMapping("/join")
-	public RsData<Void> join() {
+	public RsData<Void> join(@RequestBody JoinReqBody reqBody) {
+		memberService.join(reqBody.username(), reqBody.password(), reqBody.nickname());
 		return new RsData<>("201-1", "회원 가입이 완료되었습니다.");
 	}
 }
