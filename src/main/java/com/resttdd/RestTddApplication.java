07871e64ea87b704684e2698fb05c9cb78c15731
socra167 <socra167@gmail.com>
2025-02-04 12:41:29 +0900

    initial commit

---
+++ src/main/java/com/resttdd/RestTddApplication.java
@@ -0,0 +1,13 @@
+package com.resttdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/resttdd/RestTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resttdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
