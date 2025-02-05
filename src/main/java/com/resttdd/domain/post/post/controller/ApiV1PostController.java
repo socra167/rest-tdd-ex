26814026db6ea3a2d2b19eaf0c2cd294f9f5ee7a
socra167 <socra167@gmail.com>
2025-02-05 12:55:13 +0900

    feat: 글 작성 기능을 구현해 테스트 성공

--- src/main/java/com/resttdd/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/resttdd/domain/post/post/controller/ApiV1PostController.java
@@ -2,12 +2,16 @@ package com.resttdd.domain.post.post.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.resttdd.domain.member.member.entity.Member;
 import com.resttdd.domain.post.post.dto.PostDto;
 import com.resttdd.domain.post.post.entity.Post;
 import com.resttdd.domain.post.post.service.PostService;
+import com.resttdd.global.Rq;
 import com.resttdd.global.dto.RsData;
 import com.resttdd.global.exception.ServiceException;
 
@@ -19,6 +23,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ApiV1PostController {
 	private final PostService postService;
+	private final Rq rq;
 
 	@GetMapping("{id}")
 	public RsData<PostDto> getItem(@PathVariable long id) {
@@ -28,6 +33,22 @@ public class ApiV1PostController {
 		return new RsData<>(
 			"200-1",
 			"%d번 글을 조회하였습니다.".formatted(post.getId()),
+			new PostDto(post)
+		);
+	}
+
+	record WriteReqBody(String title, String content) {
+	}
+
+	@PostMapping
+	public RsData<PostDto> write(@RequestBody WriteReqBody body) {
+		Member actor = rq.getAuthenticatedActor();
+
+		Post post = postService.write(actor, body.title(), body.content());
+
+		return new RsData<>(
+			"201-1",
+			"%d번 글 작성이 완료되었습니다.".formatted(post.getId()),
 			new PostDto(post)
 		);
 	}
