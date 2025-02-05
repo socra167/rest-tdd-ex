a15bd4b38dc8f3ef3cc09f5ec963151fe018cbcb
socra167 <socra167@gmail.com>
2025-02-05 11:15:11 +0900

    feat: 존재하지 않는 아이디로 로그인하면 실패 응답하도록 구현해 테스트 성공

--- src/main/java/com/resttdd/domain/member/member/controller/ApiV1MemberController.java
+++ src/main/java/com/resttdd/domain/member/member/controller/ApiV1MemberController.java
@@ -47,7 +47,8 @@ public class ApiV1MemberController {
 
 	@PostMapping("/login")
 	public RsData<LoginResBody> login(@RequestBody LoginReqBody body) {
-		Member member = memberService.findByUsername(body.username()).get();
+		Member member = memberService.findByUsername(body.username())
+			.orElseThrow(() -> new ServiceException("401-1", "잘못된 아이디입니다."));
 
 		if (!member.getPassword().equals(body.password)) {
 			throw new ServiceException("401-2", "비밀번호가 일치하지 않습니다.");
--- src/main/java/com/resttdd/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/resttdd/global/exception/GlobalExceptionHandler.java
@@ -1,35 +1,18 @@
 package com.resttdd.global.exception;
 
-import com.resttdd.global.app.AppConfig;
-import com.resttdd.global.dto.RsData;
+import java.util.stream.Collectors;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
+import com.resttdd.global.app.AppConfig;
+import com.resttdd.global.dto.RsData;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
-    @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<RsData<Void>> handle(NoSuchElementException e) {
-
-        // 개발 모드에서만 작동되도록.
-        if(AppConfig.isNotProd()) e.printStackTrace();
-
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(
-                        new RsData<>(
-                                "404-1",
-                                "해당 데이터가 존재하지 않습니다"
-                        )
-                );
-    }
-
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<RsData<Void>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
