abc9899de68d573bc74b1dff905e08ba399a0ce8
socra167 <socra167@gmail.com>
2025-02-05 11:13:45 +0900

    fix: `createdDate`, `modifiedDate`의 소수점 자리수 맞춤

--- src/main/java/com/resttdd/global/entity/BaseTime.java
+++ src/main/java/com/resttdd/global/entity/BaseTime.java
@@ -1,14 +1,21 @@
 package com.resttdd.global.entity;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
-import lombok.*;
+import java.time.LocalDateTime;
+
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @MappedSuperclass
 @NoArgsConstructor
@@ -19,10 +26,12 @@ public class BaseTime extends BaseEntity {
     @CreatedDate
     @Setter(AccessLevel.PRIVATE)
     @JsonProperty("createdAt")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS")
     private LocalDateTime createdDate;
 
     @LastModifiedDate
     @Setter(AccessLevel.PRIVATE)
     @JsonProperty("modifiedAt")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS")
     private LocalDateTime modifiedDate;
 }
