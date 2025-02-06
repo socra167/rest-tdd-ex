db551c4c03b24da5c155234a7efbe3c1bc8236cc
socra167 <socra167@gmail.com>
2025-02-06 14:11:42 +0900

    chore: 불필요한 메서드 제거

--- src/test/java/com/resttdd/domain/post/comment/controller/ApiV1CommentControllerTest.java
+++ src/test/java/com/resttdd/domain/post/comment/controller/ApiV1CommentControllerTest.java
@@ -1,6 +1,5 @@
 package com.resttdd.domain.post.comment.controller;
 
-import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -15,11 +14,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.resttdd.domain.post.comment.entity.Comment;
 import com.resttdd.domain.post.post.service.PostService;
 
 @Transactional
@@ -59,19 +56,6 @@ class ApiV1CommentControllerTest {
 			.andExpect(handler().methodName("write"))
 			.andExpect(jsonPath("$.code").value("201-1"))
 			.andExpect(jsonPath("$.msg").value("%d번 댓글 작성이 완료되었습니다.".formatted(comment.getId())));
-	}
-
-	private void checkComment(ResultActions resultActions, Comment comment) throws Exception {
-		resultActions
-			.andExpect(jsonPath("$.data").exists())
-			.andExpect(jsonPath("$.data.id").value(comment.getId()))
-			.andExpect(jsonPath("$.data.content").value(comment.getContent()))
-			.andExpect(jsonPath("$.data.authorId").value(comment.getAuthor().getId()))
-			.andExpect(jsonPath("$.data.authorName").value(comment.getAuthor().getNickname()))
-			.andExpect(jsonPath("$.data.createdDate").value(
-				matchesPattern(comment.getCreatedDate().toString().replaceAll("0+$", "") + ".*")))
-			.andExpect(jsonPath("$.data.modifiedDate").value(
-				matchesPattern(comment.getModifiedDate().toString().replaceAll("0+$", "") + ".*")));
 	}
 
 	@Test
